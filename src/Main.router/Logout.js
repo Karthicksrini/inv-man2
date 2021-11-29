8d28a1a2fd6245a4ccbc182c0b28cafe5f57f126
Karthicksrini <82822054+Karthicksrini@users.noreply.github.com>
2021-11-29 07:55:04 -0800

    Update Logout.js

--- src/Main.router/Logout.js
+++ src/Main.router/Logout.js
@@ -4,6 +4,7 @@ import {useHistory} from "react-router-dom";
 function Logout(){
     const history = useHistory();
     useEffect(()=>{
+       localStorage.removeItem("token");
        history.push("/");
     },[]);
     return(
